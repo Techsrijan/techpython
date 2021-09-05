423043de1bbcf5d9aa5474668decddd3ee3465b7
Ashwani <aswanibtech@gmail.com>
2021-09-05 13:26:07 +0530

    turtle

---
+++ turtleintro.py
@@ -0,0 +1,40 @@
+from turtle import *
+t=Turtle()
+w=Screen()
+#w.bgcolor("grey")
+w.bgpic('test.gif')
+w.title("                               My turtle")
+#t.pencolor("red")
+
+t.pensize(10)
+t.shape('turtle')
+t.hideturtle()
+t.speed(1)
+def sq():
+    for i in range(4):
+        t.forward(100)
+        t.left(90)
+t.color("red","#1877f2")
+t.begin_fill()
+sq()
+t.end_fill()
+t.right(90)
+t.penup()
+t.fd(100)
+t.pendown()
+t.pencolor("green")
+sq()
+'''
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+
+for i in range(4):
+    t.forward(100)
+    t.left(90)
+'''
+done()
